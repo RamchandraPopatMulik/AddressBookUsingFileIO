69cacbd02b89c55206c703a0ee30dbb8dade324f
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-09 17:24:35 +0530

    Added File

---
+++ AddressBookUsing_File_IO/CsvHandler.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using MaxMind.Db;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsing_File_IO
+{
+    public class CsvHandler
+    {
+        private TextReader reader;
+        private TextWriter writer;
+        public void ImplementCvsDataHandling()
+        {
+            string importfilepath = @"E:\Basic Core Program\AddressBookUsing_File_IO\AddressBookUsingFileIO\AddressBookUsing_File_IO\Address.csv";
+            string exportfilepath = @"E:\Basic Core Program\AddressBookUsing_File_IO\AddressBookUsingFileIO\AddressBookUsing_File_IO\Export.csv";
+
+            using (var reader = new StreamReader(importfilepath)) ;
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Contact>().ToList();
+                Console.WriteLine("Read Data Successfully From Address.csv File");
+                foreach (Contact addressData in records)
+                {
+                    Console.Write("\t" + addressData.FirstName);
+                    Console.Write("\t" + addressData.LastName);
+                    Console.Write("\t" + addressData.Address);
+                    Console.Write("\t" + addressData.City);
+                    Console.Write("\t" + addressData.State);
+                    Console.Write("\t" + addressData.ZipCode);
+                    Console.Write("\n");
+                }
+                Console.WriteLine("Now Reading From Csv File Write to Csv ");
+                using (var writer = new StreamWriter(exportfilepath)) ;
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
--- AddressBookUsing_File_IO/Program.cs
+++ AddressBookUsing_File_IO/Program.cs
@@ -4,32 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nPlease Enter Operation : \n1.Add Contact \n2.View Saved Contcat \n 3.Edit Existing Contact \n4.Delete Existing Contact.\n5.Duplicate Checker.\n6.Search Person.\n7.Find City/State.\n8.Find Number Of Persons..\n9.Sort Entries \n10.Exit Adress Book Application");
-            var userInput = Console.ReadLine();
-            var addressBook = new AddressBook();
-
-
-            switch (userInput)
-            {
-                case "1":
-                    addressBook.StoreContact();
-                    break;
-                case "2":
-                    addressBook.DisplayAllContact();
-                    break;
-                case "3":
-                    addressBook.EditContact("");
-                    break;
-                case "4":
-                    addressBook.DeleteContact();
-                    break;
-                case "5":
-                    return;
-                default:
-                    Console.WriteLine("!!! Choose valid operation !!!");
-                    break;
-
-            }
+            Console.WriteLine(" Read Data From CSV and Write Data in CSV ");
+            CsvHandler csvDataHandling = new CsvHandler();
+            csvDataHandling.ImplementCvsDataHandling();
         }
         
     }
